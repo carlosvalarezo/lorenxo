a0d98e34748e42709625374df6b513aa66ec1da9
Carlos Valarezo Loaiza <carlos.valarezo@gmail.com>
2017-10-03 17:19:17 -0500

    Update Game.java

--- src/main/java/bowling/Game.java
+++ src/main/java/bowling/Game.java
@@ -12,28 +12,22 @@ public class Game{
     Player player;
     List<Integer> listOfResults;
     int totalScore;
-    BowlingFrame frames[] = new BowlingFrame[10];
+    List<BowlingFrame> bowlingFrame = new ArrayList<>();
 
     public Game(String name, List<Integer> listOfResults){
         this.player = new Player(name);
         this.listOfResults = listOfResults;
         //aqui debo obtener la lista de resultado de cada jugador que viene desde afuera
-        //JEff = [10,3,4,1,4,5,4,3,2,1] obtengo la lista y el nombre ya esta obtenido
+        //Jeff = [10,3,4,1,4,5,4,3,2,1] obtengo la lista y el nombre ya esta obtenido
 
     }
 
-    public void play(List<Integer> list){
-        Map<String, List<Bowler>> result;
-        result = list.stream().collect(Collectors.groupingBy(Bowler::getName));
-        result.forEach((k, v) -> {
-            player.setName(k);
-            player.
-
-            System.out.print(k + "=");
-            v.forEach(a->System.out.print(a.getPins() + ","));
-            System.out.println();
-        });
-
+    public void play(){
+    	//bowlingFrame = this.listOfResults.stream()
+    	//this.listOfResults.stream().forEach();
+        System.out.println(this.player.getName());
+        this.listOfResults.stream().forEach(i -> System.out.print("|" + i + "|"));
+        System.out.println();
     }
 
 }
